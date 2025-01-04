1a49e7817028a62dea3d1f25d9e3ab4e4f205a28
FortressTechnologiesInc <130385349+FortressTechnologiesInc@users.noreply.github.com>
2025-01-04 03:07:29 -0500

    Update HomeController.java

--- src/main/java/xyz/subho/retail/banking/controller/HomeController.java
+++ src/main/java/xyz/subho/retail/banking/controller/HomeController.java
@@ -23,8 +23,8 @@ import xyz.subho.retail.banking.service.UserService;
 @Controller
 public class HomeController implements ErrorController {
 
-	private static final String PATH = "/error";
-	
+    private static final String PATH = "/error";
+    
     @Autowired
     private UserService userService;
 
@@ -33,92 +33,63 @@ public class HomeController implements ErrorController {
 
     @RequestMapping("/")
     public String home() {
-    	
         return "redirect:/index";
-        
     }
 
     @RequestMapping("/index")
     public String index() {
-    	
         return "index";
-        
     }
 
     @GetMapping("/signup")
     public String signup(Model model) {
-    	
         User user = new User();
-
         model.addAttribute("user", user);
-
         return "signup";
-        
     }
 
     @SuppressWarnings("finally")
-	@PostMapping("/signup")
+    @PostMapping("/signup")
     public String signupPost(@ModelAttribute("user") User user, Model model) {
-
         if (userService.checkUserExists(user.getUsername(), user.getEmail())) {
 
             if (userService.checkEmailExists(user.getEmail())) {
-            	
                 model.addAttribute("emailExists", true);
-                
             }
 
             if (userService.checkUsernameExists(user.getUsername())) {
-            	
                 model.addAttribute("usernameExists", true);
-                
             }
 
             return "signup";
-            
         } else {
-        	try	{
-        	
-	            Set<UserRole> userRoles = new HashSet<>();
-	            userRoles.add(new UserRole(user, roleDao.findByName("ROLE_USER")));
-	
-	            userService.createUser(user, userRoles);
-        	}
-        	catch(Exception e)	{
-        		model.addAttribute("errorSignUp", true);
-        		e.printStackTrace();
-        	}
-        	finally	{
-        		model.addAttribute("successSignUp", true);
-        		return "redirect:/";
-        	}
-            
+            try {
+                Set<UserRole> userRoles = new HashSet<>();
+                userRoles.add(new UserRole(user, roleDao.findByName("ROLE_USER")));
+                userService.createUser(user, userRoles);
+            } catch (Exception e) {
+                model.addAttribute("errorSignUp", true);
+                e.printStackTrace();
+            } finally {
+                model.addAttribute("successSignUp", true);
+                return "redirect:/";
+            }
         }
-        
     }
 
     @GetMapping("/userFront")
     public String userFront(Principal principal, Model model) {
-    	
         User user = userService.findByUsername(principal.getName());
         CurrentAccount currentAccount = user.getCurrentAccount();
         SavingsAccount savingsAccount = user.getSavingsAccount();
 
         model.addAttribute("currentAccount", currentAccount);
         model.addAttribute("savingsAccount", savingsAccount);
-		/* model.addAttribute("user", user); */
-
         return "userFront";
-        
     }
-    
+
     @RequestMapping(value = PATH)
     public String error() {
         return "error";
-    }
-
-    @Override
-    public String getErrorPath() {
-        return PATH;
     }
 }
